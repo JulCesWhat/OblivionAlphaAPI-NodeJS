28571974080bcf751c80ac6d5ddcd80a9c07ee78
julceswhat <julceswhat@gmail.com>
2017-02-04 02:37:01 -0800

    This time the npm install should be able to to install the nodules.

--- app.js
+++ app.js
@@ -210,7 +210,6 @@ app.use(cors());
 
 var req = request('http://feeds.feedburner.com/breitbart?format=xml')
 var feedparser = new FeedParser([]);
-var feedItems = 0;
 
 req.on('error', function (error) {
   // handle any request errors
@@ -249,19 +248,18 @@ feedparser.on('readable', function () {
   var stream = this; // `this` is `feedparser`, which is a stream
   var meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
   var item;
-  console.log("************ " + feedItems + "   " + feedItems + " ************");
+
   while (item = stream.read()) {
-    console.log(item);
+    //console.log(item);
   }
-  console.log("************ " + feedItems + "   " + feedItems + " ************");
-  feedItems++;
+
 });
 
 
 
 
 app.get('/newsGalore', function(req, res){
-  console.log(req);
+  console.log("\n\nRequesting data...:)\n\n");
   res.json(data);
 
 });
