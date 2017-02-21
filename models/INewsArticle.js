1ac4654216eb3ddc772e0755f28885c6caa86dcb
Cesar Whatley <julceswhat@gmail.com>
2017-02-21 16:43:15 -0500

    Working on trying to connect the database to the server and creating a structure so that the app can grow in  the future.

--- models/INewsArticle.js
+++ models/INewsArticle.js
@@ -2,16 +2,17 @@ const mongoose = require('mongoose'),
       Schema = mongoose.Schema,
       ObjectId = Schema.ObjectId;
 
-const INewsArticleSchema = new Schema({
-  id                   : { type : Number, required: true },
+const INewsArticleSchema = new Schema();
+INewsArticleSchema.add({
+  //id                   : { type : Number, required: true },
   Category             : { type :  String, required: true, trim: true },
-  Title                : { type :  String, required: true, trim: true }
-  Image                : { type :  String, required: true, trim: true }
-  Content              : { type :  String, required: true, trim: true }
-  Time                 : { type :  String, required: true, trim: true }
-  Link                 : { type :  String, required: true, trim: true }
-  Company              : { type :  String, required: true, trim: true }
-  RelatedArticles      : [ INewsArticleSchema ]
+  Title                : { type :  String, required: true, trim: true },
+  Image                : { type :  String, required: true, trim: true },
+  Content              : { type :  String, required: true, trim: true },
+  Time                 : { type :  String, required: true, trim: true },
+  Link                 : { type :  String, required: true, trim: true },
+  Company              : { type :  String, required: true, trim: true },
+  RelatedArticles      : [ INewsArticleSchema ],
 });
 
 module.exports = mongoose.model('INewsArticle', INewsArticleSchema);
