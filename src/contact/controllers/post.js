9c5fb8ef89cb1912394aee9ae67ec9a8a68810df
julceswhat <julceswhat@gmail.com>
2017-04-05 01:00:13 -0700

    Fished doing the contact mail server section.

--- src/contact/controllers/post.js
+++ src/contact/controllers/post.js
@@ -4,14 +4,14 @@ const createEmail = require('../services/create-email');
 
 module.exports = (req, res, next) => {
   console.log(JSON.stringify(req.body));
-  const newUser = req.body;
+  const contactMsg = req.body;
   if (!req.body) {
     const err = new Error('User can\'t be added because the body is empty.');
     err.status = 400;
     return next(err);
   }
 
-  return createEmail(newUser)
+  return createEmail(contactMsg)
     .then(user => res.json(user))
     .catch(next);
 };
--- src/contact/services/create-email.js
+++ src/contact/services/create-email.js
@@ -5,49 +5,59 @@ const emailCheck = require('email-check'),
       mailer    = require('nodemailer'),
       emailConfig  = require('./../../lib/configLoader').emailConfig;
 
-module.exports = userEmail =>
+module.exports = contactMsg =>
   new Promise((resolve, reject) => {
 	//Here we will store in the dB and send it to me :)
-    emailCheck(userEmail)
+    emailCheck(contactMsg.email)
       .then(function (res) {
-        // Use Smtp Protocol to send Email
-        var smtpTransport = mailer.createTransport("SMTP",{
-          service: "Gmail",
-            auth: {
-                user: emailConfig.USER,
-                pass: emailConfig.PASS
-              
-            } 
-        });
-
-        var mail = {
-          from: "Yashwant Chavan <from@gmail.com>",
-          to: "userhere@gmail.com",
-          subject: "Send Email Using Node.js",
-          text: "Node.js New world for me",
-          html: "<b>Node.js New world for me</b>"
-        }
-
-        smtpTransport.sendMail(mail, function(error, response){
-          if (error) {
-            console.log(error);
-            reject(error);
-          } else {
-            console.log("Message sent: " + response.message);
-            resolve(True);
-          }
-
-          smtpTransport.close();
-          
-        });
+        sendEmail(contactMsg)
+          .then(resolve(true))
+          .catch(reject(err));
       })
       .catch(function(err) {
         if (err.message === 'refuse') {
           // The MX server is refusing requests from your IP address.
-          reject(err);
+          
+          this.sendEmail(contactMsg)
+            .then(resolve(true))
+            .catch(reject(err));
         } else {
           // Decide what to do with other errors.
           reject(err);
         }
       })
   });
+
+function sendEmail(contactMsg) {
+  return new Promise((resolve, reject) => {
+    // Use Smtp Protocol to send Email
+    var smtpTransport = mailer.createTransport({
+      service: "Gmail",
+      port: 465,
+      secure: true, // use TLS
+      auth: {
+        user: emailConfig.USER,
+        pass: emailConfig.PASS
+        } 
+    });
+
+    var mail = {
+      from: "Oblivion Alpha FrontEnd <wispersofoblivion@gmail.com>",
+      to: "wispersofoblivion@gmail.com",
+      subject: contactMsg.name + " <" + contactMsg.email + ">",
+      text: contactMsg.message
+    }
+
+    smtpTransport.sendMail(mail, function(error, response){
+      if (error) {
+        console.log(error);
+        smtpTransport.close();
+        reject(error);
+      } else {
+        console.log("Message sent: " + response.message);
+        smtpTransport.close();
+        resolve(True);
+      }
+    });
+  });
+};
